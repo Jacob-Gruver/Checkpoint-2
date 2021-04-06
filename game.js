ccf2558c2d678acd7b7cfe3636531e12609108bd
Jacob <gruvinbro@gmail.com>
2021-04-06 13:21:07 -0600

    init

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ let clickUpgrades = {
     pickAxe: {
         price: 10,
         quantity: 0,
-        multiplier: 2 
+        multiplier: 2
     },
     cheeseKnife: {
         price: 50,
@@ -15,12 +15,12 @@ let clickUpgrades = {
 };
 
 let pickAxePrice = clickUpgrades.pickAxe.price;
-let cheeseKnifePrice = clickUpgrades.cheeseKnife.price; 
+let cheeseKnifePrice = clickUpgrades.cheeseKnife.price;
 
 let autoUpgrades = {
     rover: {
         price: 35,
-        quantity: 0, 
+        quantity: 0,
         multiplier: 8
     },
     laser: {
@@ -29,9 +29,6 @@ let autoUpgrades = {
         multiplier: 500
     }
 };
-
-let roverPrice = autoUpgrades.rover.price;
-//let laserPrice = 
 
 let itemClickMulti = 0;
 let itemClickPrice = 0;
@@ -44,8 +41,8 @@ let totalAuto = 0;
 let totalUpgrades = 0;
 
 
-function mineRes(){
-    let multiClick = totalClick * 1; 
+function mineRes() {
+    let multiClick = totalClick * 1;
     itemClickMulti == 0 ? totalCheese++ : totalCheese += multiClick;
 
     update();
@@ -53,22 +50,22 @@ function mineRes(){
 
 function initiateAutoUpgrades() {
     totalCheese += (totalAuto * 1);
-  update();
+    update();
 }
 
 function startInterval() {
-    return collectionInterval = setInterval(initiateAutoUpgrades, 3000);
+    return setInterval(initiateAutoUpgrades, 3000);
 }
 
-function update(){
-    document.getElementById("cheese").innerText = totalCheese;
-    document.getElementById("upgrades").innerText = totalUpgrades;
-    document.getElementById("total-click").innerText = totalClick;
-    document.getElementById("total-auto").innerText = totalAuto;
-    document.getElementById("pickaxe-price").innerText = clickUpgrades.pickAxe.price;
-    document.getElementById("cheeseknife-price").innerText = clickUpgrades.cheeseKnife.price;
-    document.getElementById("rover-price").innerText = autoUpgrades.rover.price;
-    document.getElementById("laser-price").innerText = autoUpgrades.laser.price;;
+function update() {
+    document.getElementById("cheese").innerText = JSON.stringify(totalCheese);
+    document.getElementById("upgrades").innerText = JSON.stringify(totalUpgrades);
+    document.getElementById("total-click").innerText = JSON.stringify(totalClick);
+    document.getElementById("total-auto").innerText = JSON.stringify(totalAuto);
+    document.getElementById("pickaxe-price").innerText = JSON.stringify(clickUpgrades.pickAxe.price);
+    document.getElementById("cheeseknife-price").innerText = JSON.stringify(clickUpgrades.cheeseKnife.price);
+    document.getElementById("rover-price").innerText = JSON.stringify(autoUpgrades.rover.price);
+    document.getElementById("laser-price").innerText = JSON.stringify(autoUpgrades.laser.price);
 }
 
 function buyClickItem(item) {
@@ -81,7 +78,6 @@ function buyClickItem(item) {
         totalClick += itemMulti;
         clickUpgrades[item].price += 50;
         console.log(itemClickPrice, itemMulti, item);
-        //priceChange(item);
     } else {
         alert("Insufficient Funds");
     }
@@ -98,7 +94,6 @@ function buyAutoItem(item) {
         totalAuto += itemMulti;
         autoUpgrades[item].price += 55;
         console.log(itemAutoPrice, itemMulti, item);
-        //priceChange(item);
         startInterval();
     } else {
         alert("Insufficient Funds");
